9fbc804a39c24c03155db724992d88bc0d5935ab
LYChubby <f4uzi345@gmail.com>
2024-12-28 20:13:42 +0700

    Membuat Class HomeViewModel

--- app/src/main/java/com/example/mvvmserverdatabase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mvvmserverdatabase/viewmodel/HomeViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.mvvmserverdatabase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.mvvmserverdatabase.model.Mahasiswa
+import com.example.mvvmserverdatabase.repository.MahasiswaRepository
 
 sealed class HomeUiState {
     data class Success(val mhsList: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
 }
