67016db80e38c29d06e9421530a6491b52a62dca
LYChubby <f4uzi345@gmail.com>
2024-12-28 22:12:23 +0700

    Fix Error PenyediaViewModel

--- app/src/main/java/com/example/mvvmserverdatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mvvmserverdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.mvvmserverdatabase.MahasiswaApplications
 
 object PenyediaViewModel{
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
@@ -19,5 +20,5 @@ object PenyediaViewModel{
     }
 }
 
-fun CreationExtras.aplikasiKontak(): MahasiswaAplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaAplications)
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
