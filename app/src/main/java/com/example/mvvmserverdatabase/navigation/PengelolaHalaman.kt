fba7a829806ea22a5dfeb8307d6d00ee4538f0b3
LYChubby <f4uzi345@gmail.com>
2024-12-28 22:07:22 +0700

    Membuat PengelolaHalaman

---
+++ app/src/main/java/com/example/mvvmserverdatabase/navigation/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.mvvmserverdatabase.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mvvmserverdatabase.ui.view.DestinasiEntry
+import com.example.mvvmserverdatabase.ui.view.DestinasiHome
+import com.example.mvvmserverdatabase.ui.view.EntryMhsScreen
+import com.example.mvvmserverdatabase.ui.view.HomeScreen
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
