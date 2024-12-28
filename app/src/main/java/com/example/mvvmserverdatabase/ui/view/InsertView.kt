88ddd2fbdb8411685f08085d8fe4dc648c445aa9
LYChubby <f4uzi345@gmail.com>
2024-12-28 20:56:29 +0700

    Membuat Object DestinasiEntry

---
+++ app/src/main/java/com/example/mvvmserverdatabase/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.mvvmserverdatabase.ui.view
+
+import com.example.mvvmserverdatabase.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "Item_Entry"
+    override val titleRes = "Entry Mhs"
+}
