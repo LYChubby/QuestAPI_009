3a704529651c21fd30c91ed31b92fcdd90c7ac03
LYChubby <f4uzi345@gmail.com>
2024-12-28 21:06:44 +0700

    Membuat Fun EntryBody

--- app/src/main/java/com/example/mvvmserverdatabase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/mvvmserverdatabase/ui/view/InsertView.kt
@@ -1,19 +1,27 @@
 package com.example.mvvmserverdatabase.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mvvmserverdatabase.navigation.DestinasiNavigasi
 import com.example.mvvmserverdatabase.ui.customwidget.CostumeTopAppBar
+import com.example.mvvmserverdatabase.ui.viewmodel.InsertUiEvent
+import com.example.mvvmserverdatabase.ui.viewmodel.InsertUiState
 import com.example.mvvmserverdatabase.ui.viewmodel.InsertViewModel
 import kotlinx.coroutines.launch
 
@@ -59,3 +67,29 @@ fun EntryMhsScreen(
         )
     }
 }
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(26.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
