36587823593dca8a85d5744e61155886496a6442
LYChubby <f4uzi345@gmail.com>
2024-12-28 22:13:44 +0700

    Memanggil MahasiswaApp di MainActivity

--- app/src/main/java/com/example/mvvmserverdatabase/MainActivity.kt
+++ app/src/main/java/com/example/mvvmserverdatabase/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mvvmserverdatabase
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,17 +14,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvmserverdatabase.ui.theme.MVVMServerDatabaseTheme
+import com.example.mvvmserverdatabase.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MVVMServerDatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MahasiswaApp(
                     )
                 }
             }
