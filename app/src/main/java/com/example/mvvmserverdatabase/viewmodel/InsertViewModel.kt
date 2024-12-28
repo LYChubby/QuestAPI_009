ca6475e63582ef0ac831d99557f4b8721bb8df9e
LYChubby <f4uzi345@gmail.com>
2024-12-28 20:02:37 +0700

    Membuat Fun InsertUiEvent.toMhs

--- app/src/main/java/com/example/mvvmserverdatabase/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/mvvmserverdatabase/viewmodel/InsertViewModel.kt
@@ -4,7 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmserverdatabase.model.Mahasiswa
 import com.example.mvvmserverdatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private val mhs: MahasiswaRepository) : ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
@@ -36,4 +39,13 @@ data class InsertUiEvent(
     val jenisKelamin: String = "",
     val kelas: String = "",
     val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
 )
