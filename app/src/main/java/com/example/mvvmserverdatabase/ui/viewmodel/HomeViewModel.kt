aed937f2677a3cce4f0788a5795f99d51eb11605
LYChubby <f4uzi345@gmail.com>
2024-12-28 22:22:11 +0700

    Perbaikan Penulisan

--- app/src/main/java/com/example/mvvmserverdatabase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mvvmserverdatabase/ui/viewmodel/HomeViewModel.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.launch
 import java.io.IOException
 
 sealed class HomeUiState {
-    data class Success(val mhsList: List<Mahasiswa>) : HomeUiState()
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
 }
