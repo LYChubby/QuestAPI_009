4199b997d45a9fd0b3ac851ef68a7ac5be26395f
LYChubby <f4uzi345@gmail.com>
2024-12-28 21:14:12 +0700

    Membuat Fun Object DestinasiHome

---
+++ app/src/main/java/com/example/mvvmserverdatabase/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.mvvmserverdatabase.ui.view
+
+import com.example.mvvmserverdatabase.navigation.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
