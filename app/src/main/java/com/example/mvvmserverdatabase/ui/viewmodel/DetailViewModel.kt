68de4078d378350ca732198474c5bc1544546d80
LYChubby <f4uzi345@gmail.com>
2024-12-29 21:30:23 +0700

    Membuat DetailViewModel

--- app/src/main/java/com/example/mvvmserverdatabase/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/mvvmserverdatabase/ui/viewmodel/DetailViewModel.kt
@@ -1,27 +1,68 @@
 package com.example.mvvmserverdatabase.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvvmserverdatabase.model.Mahasiswa
+import com.example.mvvmserverdatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
 
-data class DetailUiState(
-    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
-    val isLoading: Boolean = false,
-    val isError: Boolean = false,
-    val errorMessage: String = ""
-) {
-    val isUiEventEmpty: Boolean
-        get() = detailUiEvent == InsertUiEvent()
-
-    val isUiEventNotEmpty: Boolean
-        get() = detailUiEvent != InsertUiEvent()
+sealed class DetailUiState {
+    data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
+    object Error : DetailUiState()
+    object Loading : DetailUiState()
 }
 
-fun Mahasiswa.toDetailUiEvent(): InsertUiEvent {
-    return InsertUiEvent(
-        nim = nim,
-        nama = nama,
-        alamat = alamat,
-        jenisKelamin = jenisKelamin,
-        kelas = kelas,
-        angkatan = angkatan
-    )
+class DetailViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var detailUiState: DetailUiState by mutableStateOf(DetailUiState.Loading)
+        private set
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getMhsByNim(nim: String) {
+        viewModelScope.launch {
+            detailUiState = DetailUiState.Loading
+            detailUiState = try {
+                val mahasiswa = mhs.getMahasiswaByNim(nim)
+                DetailUiState.Success(mahasiswa)
+            } catch (e: IOException) {
+                DetailUiState.Error
+            } catch (e: HttpException) {
+                DetailUiState.Error
+            }
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun updateMhs(nim: String, updatedMahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(nim, updatedMahasiswa)
+                detailUiState = DetailUiState.Success(updatedMahasiswa)
+            } catch (e: IOException) {
+                detailUiState = DetailUiState.Error
+            } catch (e: HttpException) {
+                detailUiState = DetailUiState.Error
+            }
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+                detailUiState = DetailUiState.Loading
+            } catch (e: IOException) {
+                detailUiState = DetailUiState.Error
+            } catch (e: HttpException) {
+                detailUiState = DetailUiState.Error
+            }
+        }
+    }
 }
